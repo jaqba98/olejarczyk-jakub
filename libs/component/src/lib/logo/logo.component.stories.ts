cb55e0499fe78edb734541c39bce7179d9bda015
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-04 21:15:47 +0100

    refactor: logo component

--- libs/component/src/lib/logo/logo.component.stories.ts
+++ libs/component/src/lib/logo/logo.component.stories.ts
@@ -3,14 +3,11 @@ import type { Meta, StoryObj } from '@storybook/angular';
 import { LogoComponent } from './logo.component';
 
 const meta: Meta<LogoComponent> = {
+  title: 'Logo',
   component: LogoComponent,
 };
 
 export default meta;
 type Story = StoryObj<LogoComponent>;
 
-export const Default: Story = {
-  args: {
-    title: 'Jakub Olejarczyk',
-  },
-};
+export const Default: Story = {};
--- libs/component/src/lib/logo/logo.component.ts
+++ libs/component/src/lib/logo/logo.component.ts
@@ -1,10 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { MediaEnum, MediaService } from '@olejarczyk-jakub/system';
 
 @Component({
   selector: 'lib-logo',
   templateUrl: './logo.component.html',
   styleUrl: './logo.component.scss',
 })
-export class LogoComponent {
-  @Input({ required: true }) title!: string;
+export class LogoComponent implements OnInit, OnDestroy {
+  title!: string;
+
+  private readonly fullname = 'Jakub Olejarczyk';
+
+  private readonly initials = 'JO';
+
+  private sub!: Subscription;
+
+  constructor(private readonly media: MediaService) {}
+
+  ngOnInit() {
+    this.sub = this.media.media$.subscribe((media) => {
+      this.title = this.media.lessOrEqual(media, MediaEnum.mobileSmall)
+        ? this.initials
+        : this.fullname;
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
