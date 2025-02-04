be74a40ea5b6a5e84c3301af85ca0fdfe0e64027
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-04 17:23:26 +0100

    refactor: logo component

--- libs/component/src/lib/logo/logo.component.ts
+++ libs/component/src/lib/logo/logo.component.ts
@@ -1,9 +1,7 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'lib-logo',
-  imports: [CommonModule],
   templateUrl: './logo.component.html',
   styleUrl: './logo.component.scss',
 })
