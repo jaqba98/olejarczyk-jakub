f1f687a3ff6b6b88735e71d2148e67c6cde02fc1
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-09 13:18:17 +0100

    feat: switch between hamburger and menu dependeing on media

--- libs/component/src/lib/base/base.component.ts
+++ libs/component/src/lib/base/base.component.ts
@@ -2,12 +2,19 @@ import { Component, OnDestroy } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { Subscription } from 'rxjs';
 
+import { MediaEnum } from '@olejarczyk-jakub/model';
+import { SystemState } from '@olejarczyk-jakub/store';
+import { mediaLessOrEqual } from '@olejarczyk-jakub/system';
+
 @Component({ template: '' })
 export class BaseComponent implements OnDestroy {
   private sub: Subscription;
 
+  protected isMobile = true;
+
   constructor(protected readonly store: Store) {
     this.sub = new Subscription();
+    this.initBaseComponent();
   }
 
   addSub(sub: Subscription) {
@@ -16,5 +23,13 @@ export class BaseComponent implements OnDestroy {
 
   ngOnDestroy(): void {
     this.sub.unsubscribe();
+  }
+
+  private initBaseComponent() {
+    this.addSub(
+      this.store.select(SystemState.getMedia).subscribe((media) => {
+        this.isMobile = mediaLessOrEqual(media, MediaEnum.mobileLarge);
+      })
+    );
   }
 }
--- libs/component/src/lib/view/nav-view/nav-view.component.ts
+++ libs/component/src/lib/view/nav-view/nav-view.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngxs/store';
 
 import { LogoSmartComponent } from '../../smart/logo-smart/logo-smart.component';
 import { HamburgerSmartComponent } from '../../smart/hamburger-smart/hamburger-smart.component';
 import { MenuViewComponent } from '../menu-view/menu-view.component';
+import { BaseComponent } from '../../base/base.component';
 
 @Component({
   selector: 'lib-nav-view',
@@ -10,4 +12,8 @@ import { MenuViewComponent } from '../menu-view/menu-view.component';
   templateUrl: './nav-view.component.html',
   styleUrl: './nav-view.component.scss',
 })
-export class NavViewComponent {}
+export class NavViewComponent extends BaseComponent {
+  constructor(protected override readonly store: Store) {
+    super(store);
+  }
+}
