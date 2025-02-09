f2e817d6bea37bc3a454f92f6f1cad18a590148b
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-09 10:53:25 +0100

    fix: menu component

--- libs/component/src/lib/menu/menu.component.ts
+++ libs/component/src/lib/menu/menu.component.ts
@@ -1,9 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MediaService } from '@olejarczyk-jakub/system';
 import { ButtonComponent } from '../button/button.component';
-import { BaseComponent } from '../base/base.component';
 
 @Component({
   selector: 'lib-menu',
@@ -11,12 +9,8 @@ import { BaseComponent } from '../base/base.component';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
-export class MenuComponent extends BaseComponent<void> {
+export class MenuComponent {
   @Input() horizontal = false;
 
   isVisible = false;
-
-  constructor(protected override readonly media: MediaService) {
-    super(media);
-  }
 }
