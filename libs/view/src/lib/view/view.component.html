d3dfe8d1d2725d18925754796347ebef9b95fde8
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-10 22:36:42 +0100

    feat: create an empty view project

---
+++ libs/view/src/lib/view/view.component.html
@@ -0,0 +1,1 @@
+<p>View works!</p>
---
+++ libs/view/src/lib/view/view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-view',
+  imports: [CommonModule],
+  templateUrl: './view.component.html',
+  styleUrl: './view.component.css',
+})
+export class ViewComponent {}
