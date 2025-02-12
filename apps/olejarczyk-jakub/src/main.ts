b9e6ff42ad8be6ca3719566ab7000cf673f1c613
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-12 15:23:06 +0100

    refactor: application styles

--- apps/olejarczyk-jakub/src/main.ts
+++ apps/olejarczyk-jakub/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 
 import { RootComponent } from '@olejarczyk-jakub/page';
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(RootComponent, appConfig).catch((err) =>
   console.error(err)
