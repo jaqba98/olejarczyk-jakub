2fde931c33113d808656b9b89e9759c4331ad913
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-12 16:42:39 +0100

    feat: display text inside text dumb component but only p

--- libs/component/src/lib/dumb/text-dumb/text-dumb.component.stories.ts
+++ libs/component/src/lib/dumb/text-dumb/text-dumb.component.stories.ts
@@ -7,6 +7,15 @@ const meta: Meta<TextDumbComponent> = {
   title: 'Component/Dumb/Text',
   component: TextDumbComponent,
   ...StorybookUtils.decorators(),
+  render: (args) => ({
+    props: args,
+    template: `
+    <lib-text-dumb>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro eaque in commodi ea,
+      explicabo aut at distinctio nesciunt laborum praesentium asperiores numquam facere 
+      error iusto est libero provident impedit? Iusto.
+    </lib-text-dumb>`,
+  }),
 };
 
 export default meta;
