780609e42059ca58fda61b298a2532ec6e86e27d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-13 17:46:57 +0100

    refactor: home view component

--- libs/component/src/lib/view/home-view/home-view.component.ts
+++ libs/component/src/lib/view/home-view/home-view.component.ts
@@ -4,12 +4,14 @@ import { Component } from '@angular/core';
 import { LogoEnum, profession, SectionEnum } from '@olejarczyk-jakub/model';
 import { BaseViewComponent } from '../base-view/base-view.component';
 import { TextSmartComponent } from '../../smart/text-smart/text-smart.component';
+import { StyleOffDirective } from '../../base/style-off.directive';
 
 @Component({
   selector: 'lib-home-view',
   imports: [CommonModule, BaseViewComponent, TextSmartComponent],
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.scss',
+  hostDirectives: [StyleOffDirective],
 })
 export class HomeViewComponent {
   readonly id = SectionEnum.home;
