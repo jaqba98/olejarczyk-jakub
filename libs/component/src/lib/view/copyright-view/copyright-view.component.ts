2d5895748203ac2466032148f767126cbf7817c4
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-14 20:03:41 +0100

    refactor: copyright view

--- libs/component/src/lib/view/copyright-view/copyright-view.component.ts
+++ libs/component/src/lib/view/copyright-view/copyright-view.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
-import { BaseComponent } from '../../base/base.component';
+import { LogoEnum } from '@olejarczyk-jakub/model';
 import { BaseViewComponent } from '../base-view/base-view.component';
+import { TextSmartComponent } from '../../smart/text-smart/text-smart.component';
 
 @Component({
   selector: 'lib-copyright-view',
-  imports: [CommonModule, BaseViewComponent],
+  imports: [CommonModule, BaseViewComponent, TextSmartComponent],
   templateUrl: './copyright-view.component.html',
   styleUrl: './copyright-view.component.scss',
 })
-export class CopyrightViewComponent extends BaseComponent {}
+export class CopyrightViewComponent {
+  readonly logo = LogoEnum.full;
+
+  getCopyright() {
+    return `© 2025 ${this.logo}. All Rights Reserved.`;
+  }
+}
