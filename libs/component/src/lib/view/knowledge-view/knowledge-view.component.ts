6a503fcd0faef6e695bbebdfa15a71fabdae651b
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-02-14 12:49:08 +0100

    refactor: knowledge section but without icons

--- libs/component/src/lib/view/knowledge-view/knowledge-view.component.ts
+++ libs/component/src/lib/view/knowledge-view/knowledge-view.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
+import { SectionEnum } from '@olejarczyk-jakub/model';
 import { BaseViewComponent } from '../base-view/base-view.component';
 import { TitleViewComponent } from '../title-view/title-view.component';
-import { BaseComponent } from '../../base/base.component';
-import { SectionEnum } from '@olejarczyk-jakub/model';
+import { TextSmartComponent } from '../../smart/text-smart/text-smart.component';
 
 @Component({
   selector: 'lib-knowledge-view',
-  imports: [CommonModule, BaseViewComponent, TitleViewComponent],
+  imports: [
+    CommonModule,
+    BaseViewComponent,
+    TitleViewComponent,
+    TextSmartComponent,
+  ],
   templateUrl: './knowledge-view.component.html',
   styleUrl: './knowledge-view.component.scss',
 })
-export class KnowledgeViewComponent extends BaseComponent {
-  id = SectionEnum.knowledge;
+export class KnowledgeViewComponent {
+  readonly id = SectionEnum.knowledge;
 }
