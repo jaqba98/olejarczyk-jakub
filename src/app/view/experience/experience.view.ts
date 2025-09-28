b537f0f190f381c6c34cae393d504cc006fe42c9
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-09-28 14:14:12 +0200

    Create view experience component

---
+++ src/app/view/experience/experience.view.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'view-experience',
+  imports: [],
+  templateUrl: './experience.view.html',
+  styleUrl: './experience.view.scss',
+})
+export class ExperienceView {}
