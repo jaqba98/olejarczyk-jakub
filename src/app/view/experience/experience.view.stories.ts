509517d62c5606e9f39294bc25891c5e956a87c2
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-09-29 08:42:22 +0200

    Add stories file and set up the ngxs in the storybook

---
+++ src/app/view/experience/experience.view.stories.ts
@@ -0,0 +1,26 @@
+import { applicationConfig, moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { provideStore } from '@ngxs/store';
+
+import { ExperienceView } from './experience.view';
+import { CompanyState } from '../../state/company.state';
+
+const meta: Meta<ExperienceView> = {
+  component: ExperienceView,
+  decorators: [
+    applicationConfig({
+      providers: [provideStore([CompanyState], withNgxsReduxDevtoolsPlugin())],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<ExperienceView>;
+
+export const Primary: Story = {
+  decorators: [
+    moduleMetadata({
+      providers: [],
+    }),
+  ],
+};
