faf75f0a9fca22a0dff6f90b84299919dcd47279
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-02 13:38:20 +0200

    Cahnge text to paragraph

---
+++ src/app/component/paragraph/paragraph.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'component-paragraph',
+  templateUrl: './paragraph.component.html',
+  styleUrl: './paragraph.component.scss',
+})
+export class ParagraphComponent {}
--- src/app/component/text/text.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'component-text',
-  templateUrl: './text.component.html',
-  styleUrl: './text.component.scss',
-})
-export class TextComponent {}
