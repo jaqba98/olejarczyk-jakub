2c31020582d99ef8a03de6c86f341c0ade4603c1
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-03 19:32:03 +0200

    Dispaly the content of the experience view

--- src/app/component/view/experience/experience-view.component.stories.ts
+++ src/app/component/view/experience/experience-view.component.stories.ts
@@ -1,41 +1,36 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { provideStore } from '@ngxs/store';
+import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
 
 import { ExperienceViewComponent } from './experience-view.component';
+import { CompanyState } from '../../../store/company/company.state';
+import { ExperienceState } from '../../../store/experience/experience.state';
+import { TechnologyCategoryState } from '../../../store/technology-category/technology-category.state';
+import { TechnologyGroupState } from '../../../store/technology-group/technology-group.state';
+import { TechnologyState } from '../../../store/technology/technology.state';
 
 const meta: Meta<ExperienceViewComponent> = {
   title: 'Component/View/Experience',
   component: ExperienceViewComponent,
+  decorators: [
+    applicationConfig({
+      providers: [
+        provideStore(
+          [
+            CompanyState,
+            ExperienceState,
+            TechnologyState,
+            TechnologyGroupState,
+            TechnologyCategoryState,
+          ],
+          withNgxsReduxDevtoolsPlugin(),
+        ),
+      ],
+    }),
+  ],
 };
 
 export default meta;
 type Story = StoryObj<ExperienceViewComponent>;
 
 export const Primary: Story = {};
-
-// const meta: Meta<ExperienceViewComponent> = {
-//   component: ExperienceViewComponent,
-//   parameters: {
-//     layout: 'fullscreen',
-//   },
-//   decorators: [
-//     applicationConfig({
-//       providers: [
-//         provideStore(
-//           [
-//             CompanyState,
-//             ExperienceState,
-//             TechnologyState,
-//             TechnologyGroupState,
-//             TechnologyCategoryState,
-//           ],
-//           withNgxsReduxDevtoolsPlugin(),
-//         ),
-//       ],
-//     }),
-//   ],
-// };
-
-// export default meta;
-// type Story = StoryObj<ExperienceViewComponent>;
-
-// export const Primary: Story = {};
--- src/app/component/view/experience/experience-view.component.ts
+++ src/app/component/view/experience/experience-view.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TextDumbComponent } from '../../dumb/text/text-dumb.component';
+import { ExperienceViewService } from './experience-view.service';
 
 @Component({
   selector: 'experience-view',
   templateUrl: './experience-view.component.html',
   styleUrl: './experience-view.component.scss',
+  imports: [CommonModule, TextDumbComponent],
+  providers: [ExperienceViewService],
 })
-export class ExperienceViewComponent {}
+export class ExperienceViewComponent {
+  experiences$;
+
+  constructor(private readonly experienceViewService: ExperienceViewService) {
+    this.experiences$ = this.experienceViewService.getExperiences();
+  }
+}
