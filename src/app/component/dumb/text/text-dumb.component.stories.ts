23f907e3aec9670c29c81435ee65c4dce9fb09b3
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-03 18:42:07 +0200

    Add ng content to the text dumb component

--- src/app/component/dumb/text/text-dumb.component.stories.ts
+++ src/app/component/dumb/text/text-dumb.component.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 
 import { TextDumbComponent } from './text-dumb.component';
+import { TextDumbType } from './text-dumb.type';
 
 const meta: Meta<TextDumbComponent> = {
   title: 'Component/Dumb/Text',
@@ -10,66 +11,99 @@ const meta: Meta<TextDumbComponent> = {
 export default meta;
 type Story = StoryObj<TextDumbComponent>;
 
+const buildTextDumb = (type: TextDumbType, value: string) => {
+  return `<text-dumb type="${type}"><span ${type}>${value}</span></text-dumb>`;
+};
+
 export const P: Story = {
+  render: (args) => ({
+    props: args,
+    template: buildTextDumb('p', 'p works!'),
+  }),
   args: {
     type: 'p',
-    value: 'p works',
   },
 };
 
 export const H1: Story = {
+  render: (args) => ({
+    props: args,
+    template: buildTextDumb('h1', 'h1 works!'),
+  }),
   args: {
     type: 'h1',
-    value: 'h1 works',
   },
 };
 
 export const H2: Story = {
+  render: (args) => ({
+    props: args,
+    template: buildTextDumb('h2', 'h2 works!'),
+  }),
   args: {
     type: 'h2',
-    value: 'h2 works',
   },
 };
 
 export const H3: Story = {
+  render: (args) => ({
+    props: args,
+    template: buildTextDumb('h3', 'h3 works!'),
+  }),
   args: {
     type: 'h3',
-    value: 'h3 works',
   },
 };
 
 export const H4: Story = {
+  render: (args) => ({
+    props: args,
+    template: buildTextDumb('h4', 'h4 works!'),
+  }),
   args: {
     type: 'h4',
-    value: 'h4 works',
   },
 };
 
 export const H5: Story = {
+  render: (args) => ({
+    props: args,
+    template: buildTextDumb('h5', 'h5 works!'),
+  }),
   args: {
     type: 'h5',
-    value: 'h5 works',
   },
 };
 
 export const H6: Story = {
+  render: (args) => ({
+    props: args,
+    template: buildTextDumb('h6', 'h6 works!'),
+  }),
   args: {
     type: 'h6',
-    value: 'h6 works',
   },
 };
 
 export const Small: Story = {
+  render: (args) => ({
+    props: args,
+    template: buildTextDumb('small', 'small works!'),
+  }),
   args: {
     type: 'small',
-    value: 'small works',
   },
 };
 
 export const LongText: Story = {
+  render: (args) => ({
+    props: args,
+    template: buildTextDumb(
+      'p',
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti ducimus repellat, aut omnis odit quaerat aliquam laborum doloribus repudiandae quis consequuntur iure praesentium nostrum deserunt tempora asperiores corporis cupiditate, unde veniam aliquid aspernatur hic. Qui quae praesentium delectus voluptatibus optio repellat voluptates, animi explicabo cupiditate, quo, itaque temporibus! Nulla accusamus iusto debitis omnis, necessitatibus neque error. Officiis expedita maxime asperiores harum accusantium, praesentium sint ratione veniam doloremque, incidunt quas. Consequuntur, dicta blanditiis deserunt tempore aperiam alias, reiciendis similique quia quibusdam dignissimos nulla sint nobis perferendis error unde ipsum quam commodi ex cum, facere nisi optio facilis quod ullam. Corporis qui, ducimus quibusdam molestiae unde delectus dignissimos praesentium tempore eveniet nam sed animi! Odio, possimus rerum, sunt amet provident asperiores deleniti quibusdam ex cupiditate quaerat facere neque. Inventore minus mollitia doloribus cum quibusdam autem tempore esse possimus, ducimus expedita quidem voluptate cupiditate incidunt ratione repellat animi nemo veritatis porro id officiis, ipsum neque obcaecati asperiores? Voluptates eaque ut ullam ducimus officia aperiam saepe, minus voluptate, soluta reiciendis maiores illum fugit eos. Quisquam ea et nulla aut. Minus magnam optio non assumenda itaque laboriosam sapiente inventore, vero quia id commodi iusto natus. Facilis ipsam molestiae a neque, illum hic in. Dicta ipsam voluptatem suscipit facilis vero, hic dolore aperiam odio? Laboriosam tempore beatae eum sint laborum assumenda modi, magni nostrum, esse delectus provident adipisci. Porro labore vero officia aspernatur amet quasi ex quas non, ipsa repudiandae quidem optio culpa unde soluta, distinctio neque odio eum magni id minima illum quo voluptatum molestiae? Libero id quibusdam impedit pariatur illum odio, consectetur facilis vitae delectus quos natus maiores earum neque dolorem officia excepturi mollitia aperiam obcaecati est eligendi nesciunt sunt consequatur. Eaque dignissimos odio ab eligendi harum vero corporis perferendis ipsam iste quaerat in aspernatur distinctio neque fuga, culpa architecto cupiditate praesentium omnis voluptatum expedita autem sunt fugiat assumenda. Adipisci error illo odio omnis quo, ipsam corrupti eveniet temporibus architecto ipsa fugit quod eligendi ex neque pariatur a recusandae fugiat ducimus voluptas provident doloribus? Saepe optio excepturi, animi facilis placeat, repellendus dolores aliquid earum vitae est deserunt voluptates quos nobis, laborum mollitia incidunt accusantium ex aliquam. Doloremque reprehenderit quia sunt laudantium omnis accusamus eveniet asperiores odit in. Sunt quaerat repellat quam et! Rerum, dicta. Quasi, modi? Nostrum, ullam? Quos iusto suscipit amet soluta commodi, tempora blanditiis hic voluptate, doloribus optio explicabo, doloremque ipsa! Voluptate doloremque recusandae aut ratione facere a magnam voluptatum saepe. Vero porro distinctio in tempore, sit saepe nulla aliquam libero iure et quaerat optio veniam nemo dolore! Eius ex facilis aut aspernatur doloribus deleniti repellat explicabo natus officia doloremque amet mollitia corrupti deserunt optio consectetur rem libero, molestiae sunt numquam exercitationem porro cum nulla. Voluptates beatae reiciendis nam officiis minus similique nostrum molestias, tenetur quidem, adipisci nisi! Distinctio maiores at quaerat, temporibus aliquam nisi nostrum fugit totam, facilis, amet officia quos eveniet ex esse omnis ipsum non dolore quam nemo consectetur adipisci impedit quo repudiandae error. Sed voluptas nisi commodi! Magnam culpa dolores facilis eligendi quisquam hic est quibusdam, porro veniam ipsa non consequuntur illo nobis accusamus expedita totam quo eveniet. Expedita possimus sunt esse, error iste non, tempora distinctio illum aperiam nemo, perspiciatis omnis. Dolores rem dolorum sapiente quos culpa quibusdam repellat rerum deserunt eos sunt ut architecto blanditiis, at explicabo voluptatibus temporibus? Eius suscipit esse consectetur exercitationem fugiat labore ipsam expedita incidunt. Possimus illo repellendus nemo nisi ad eveniet, cum ratione. Quam molestias culpa inventore odio hic esse corrupti id ab voluptates eveniet ipsa vel quia dicta, ipsam recusandae nisi explicabo quos, consequuntur et distinctio alias. Expedita maxime alias natus amet sequi, provident similique officia unde? Quidem sapiente hic aspernatur eaque iusto placeat cupiditate optio accusantium error nulla. Consequuntur, molestiae, quod minus eligendi quisquam qui nisi quasi magni hic non, earum alias repellendus sit deserunt quae culpa mollitia ullam officiis debitis cumque iste nobis doloremque voluptatibus veritatis. Consequatur nostrum tempore reiciendis dolor, quasi laudantium nam eius quidem mollitia odit totam, vel repudiandae recusandae beatae nesciunt vitae tempora nemo, explicabo ipsa eligendi molestiae. Nihil, reprehenderit eum sequi officiis voluptatum repellat doloribus delectus. Sequi, quos ex temporibus harum dignissimos saepe placeat minima quis incidunt, accusantium in voluptates voluptatem sunt. Doloremque hic ipsam, nostrum officiis dolore aspernatur impedit quasi temporibus incidunt ratione numquam. Sequi tenetur inventore ipsa dicta deleniti laboriosam, cumque eveniet similique fugit veritatis, id suscipit earum eius, pariatur accusantium quae provident et ab. Enim deserunt voluptatum culpa dolore soluta in eligendi facilis excepturi unde ea. Itaque rem aliquam quod repellendus quisquam laboriosam illo, voluptas dignissimos rerum vero sit, aliquid tempore. Mollitia rerum dolorem quod dolorum officia, cum, maxime veritatis eius amet architecto temporibus doloribus omnis quibusdam doloremque est, vel accusamus nam minus illo iure sapiente? Illum recusandae asperiores in nulla repellendus, eum itaque voluptate nobis delectus reprehenderit natus necessitatibus neque ullam officiis sint vel consequatur quaerat corrupti? Delectus aspernatur eligendi quia sed ullam harum velit, dolorum excepturi numquam molestias est perferendis alias ipsum nihil. Perspiciatis delectus vitae, facere recusandae corporis veritatis, dolor est quia cum architecto laborum ex eum impedit dolorum aspernatur eos autem doloremque odit nihil cumque? Tenetur ipsam vero, doloribus unde beatae blanditiis dolore fuga ad. Aliquam dolores quia commodi cupiditate eveniet harum quae, nemo autem voluptatibus architecto asperiores soluta quam repellendus molestiae minus sapiente, nihil dolore. Nulla totam eos maxime minus tenetur enim! Aliquid, nisi odio libero rem similique, possimus fuga reiciendis saepe obcaecati distinctio, inventore nemo expedita facilis. Laborum voluptatum eaque odio inventore ipsa porro corporis alias unde quae at, architecto tenetur magni tempore impedit saepe aspernatur aliquam ullam illum deserunt. Reiciendis eligendi laborum nemo animi beatae magni officiis sed obcaecati odit sapiente eveniet ducimus dignissimos ut, laudantium numquam, commodi sunt veniam recusandae temporibus quo vitae iure dolor! Id, modi? Doloribus commodi voluptates sequi, ut saepe nobis porro reiciendis pariatur incidunt praesentium laboriosam autem accusantium, culpa animi soluta architecto corrupti? Explicabo iste quasi molestiae voluptatum cupiditate reiciendis pariatur nam enim! Quis doloremque voluptas impedit sequi quas saepe voluptatem totam aut facere dicta enim velit in, iure quo dolores quibusdam rem cum animi magni sed minus unde!',
+    ),
+  }),
   args: {
     type: 'p',
-    value:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo eget odio non aliquet. Integer sit amet nunc leo. Proin odio tortor, cursus id ultricies eget, sagittis non nunc. Suspendisse auctor, lorem in efficitur consequat, elit risus facilisis ex, vitae pellentesque eros risus id erat. In vulputate orci odio. Nam nec fermentum mi, in commodo est. Morbi quis venenatis tortor. Curabitur sed turpis ante.  Pellentesque eget mauris a est ullamcorper lobortis. Donec eget purus eu tellus egestas facilisis sit amet sit amet nisi. Nulla facilisi. Nunc nunc ex, ornare commodo dignissim sit amet, varius vel sem. Ut metus nisl, iaculis nec tincidunt ut, tempus nec tortor. Sed posuere mi orci, id mattis ligula luctus eget. Maecenas blandit sit amet tortor vitae congue. Proin rutrum condimentum turpis vel fringilla. Pellentesque ac ex in justo posuere facilisis. Donec interdum nisl ante, ac varius felis fermentum non. Praesent ullamcorper est odio, sit amet euismod ex laoreet eget. Aenean orci tortor, pulvinar vel volutpat ac, posuere ac dui. Pellentesque at est hendrerit sapien dignissim venenatis. Phasellus ullamcorper leo est, euismod commodo purus dapibus a.  Aliquam bibendum nulla nibh, scelerisque faucibus felis maximus sed. Maecenas sem felis, facilisis sit amet mi in, cursus tristique lorem. Curabitur luctus erat ut mi cursus, quis finibus justo sodales. Cras lobortis cursus augue, vitae egestas neque venenatis ac. Nam a est congue, tincidunt nisi et, tempor nisl. Phasellus tempor vehicula rutrum. In placerat nibh eget nibh molestie, sit amet tristique augue varius. Curabitur vehicula orci iaculis leo cursus, at pellentesque nisi pellentesque. Nam id accumsan dui. Aenean justo justo, eleifend non odio nec, tincidunt vehicula felis. Duis a metus sed nibh aliquet tristique.  Etiam ante diam, ullamcorper eu scelerisque a, viverra vel quam. Donec vulputate tempor pharetra. Praesent elementum, odio vel tincidunt tempus, ipsum nunc maximus tellus, vitae facilisis odio felis id nunc. Praesent sed est nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis tellus suscipit tortor luctus lobortis. Suspendisse consequat tortor mauris, sit amet sollicitudin diam consequat eu. Donec malesuada lacus eget magna tempor viverra. Nullam id gravida metus.',
   },
 };
--- src/app/component/dumb/text/text-dumb.component.ts
+++ src/app/component/dumb/text/text-dumb.component.ts
@@ -3,12 +3,10 @@ import { Component, input } from '@angular/core';
 import { TextDumbType } from './text-dumb.type';
 
 @Component({
-  selector: 'component-text-dumb',
+  selector: 'text-dumb',
   templateUrl: './text-dumb.component.html',
   styleUrl: './text-dumb.component.scss',
 })
 export class TextDumbComponent {
   type = input.required<TextDumbType>();
-
-  value = input.required<string>();
 }
