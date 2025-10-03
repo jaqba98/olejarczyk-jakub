c43e24a61cdabe1166b19492f53b1e52322e0fd4
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-03 17:05:31 +0200

    Use text in the view

--- src/app/view/experience/experience.view.ts
+++ src/app/view/experience/experience.view.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { ExperienceService } from './experience.service';
+import { TextDumbComponent } from '../../component/dumb/text/text-dumb.component';
 
 @Component({
   selector: 'view-experience',
   templateUrl: './experience.view.html',
   styleUrl: './experience.view.scss',
-  imports: [CommonModule],
+  imports: [CommonModule, TextDumbComponent],
   providers: [ExperienceService],
 })
 export class ExperienceView {
