d79763cb841d438ece10a06c8d7d2e224497c991
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-04 16:58:01 +0200

    Refactor the technology state

--- src/app/store/technology/technology.state.ts
+++ src/app/store/technology/technology.state.ts
@@ -1,367 +1,367 @@
 import { Selector, State } from '@ngxs/store';
+import { Injectable } from '@angular/core';
 
 import { TechnologyStateModel } from './technology-state.model';
-import { Injectable } from '@angular/core';
 
 @State<TechnologyStateModel>({
   name: 'technology',
   defaults: {
-    // markupLanguage: {
-    //   html: {
-    //     label: 'HTML',
-    //     level: 'advanced',
-    //     defaultGroup: 'frontend',
-    //     companies: {
-    //       aprSystem: ['frontend'],
-    //       primaris: [],
-    //     },
-    //   },
-    // },
-    // stylesheetLanguage: {
-    //   css: {
-    //     label: 'CSS',
-    //     level: 'beginner',
-    //     defaultGroup: 'frontend',
-    //     companies: {
-    //       aprSystem: ['frontend'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   scss: {
-    //     label: 'SCSS',
-    //     level: 'beginner',
-    //     defaultGroup: 'frontend',
-    //     companies: {
-    //       aprSystem: ['frontend'],
-    //       primaris: [],
-    //     },
-    //   },
-    // },
-    // programmingLanguage: {
-    //   javascript: {
-    //     label: 'JavaScript',
-    //     level: 'beginner',
-    //     defaultGroup: 'frontend',
-    //     companies: {
-    //       aprSystem: ['frontend', 'backend'],
-    //       primaris: ['integration'],
-    //     },
-    //   },
-    //   typescript: {
-    //     label: 'TypeScript',
-    //     level: 'beginner',
-    //     defaultGroup: 'frontend',
-    //     companies: {
-    //       aprSystem: ['frontend', 'backend'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   java: {
-    //     label: 'Java',
-    //     level: 'beginner',
-    //     defaultGroup: 'backend',
-    //     companies: {
-    //       aprSystem: [],
-    //       primaris: [],
-    //     },
-    //   },
-    //   python: {
-    //     label: 'Python',
-    //     level: 'beginner',
-    //     defaultGroup: 'ai',
-    //     companies: {
-    //       aprSystem: [],
-    //       primaris: [],
-    //     },
-    //   },
-    // },
-    // framework: {
-    //   angular: {
-    //     label: 'Angular',
-    //     level: 'beginner',
-    //     defaultGroup: 'frontend',
-    //     companies: {
-    //       aprSystem: ['frontend'],
-    //       primaris: [],
-    //     },
-    //   },
-    // },
-    // library: {
-    //   rxjs: {
-    //     label: 'RxJS',
-    //     level: 'beginner',
-    //     defaultGroup: 'frontend',
-    //     companies: {
-    //       aprSystem: ['frontend'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   ngxs: {
-    //     label: 'NGXS',
-    //     level: 'intermediate',
-    //     defaultGroup: 'frontend',
-    //     companies: {
-    //       aprSystem: ['frontend'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   storybook: {
-    //     label: 'Storybook',
-    //     level: 'intermediate',
-    //     defaultGroup: 'frontend',
-    //     companies: {
-    //       aprSystem: ['frontend'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   primeng: {
-    //     label: 'PrimeNG',
-    //     level: 'beginner',
-    //     defaultGroup: 'frontend',
-    //     companies: {
-    //       aprSystem: ['frontend'],
-    //       primaris: [],
-    //     },
-    //   },
-    // },
-    // test: {
-    //   jest: {
-    //     label: 'Jest',
-    //     level: 'beginner',
-    //     defaultGroup: 'frontend',
-    //     companies: {
-    //       aprSystem: ['frontend'],
-    //       primaris: [],
-    //     },
-    //   },
-    // },
-    // database: {
-    //   postgresql: {
-    //     label: 'PostgreSQL',
-    //     level: 'beginner',
-    //     defaultGroup: 'backend',
-    //     companies: {
-    //       aprSystem: ['backend'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   microsoftSqlServer: {
-    //     label: 'Microsoft SQL Server',
-    //     level: 'beginner',
-    //     defaultGroup: 'integration',
-    //     companies: {
-    //       aprSystem: [],
-    //       primaris: ['integration'],
-    //     },
-    //   },
-    // },
-    // server: {
-    //   nginx: {
-    //     label: 'NGINX',
-    //     level: 'beginner',
-    //     defaultGroup: 'devops',
-    //     companies: {
-    //       aprSystem: [],
-    //       primaris: [],
-    //     },
-    //   },
-    // },
-    // runtime: {
-    //   nodejs: {
-    //     label: 'Node.js',
-    //     level: 'beginner',
-    //     defaultGroup: 'backend',
-    //     companies: {
-    //       aprSystem: ['backend'],
-    //       primaris: [],
-    //     },
-    //   },
-    // },
-    // containerization: {
-    //   docker: {
-    //     label: 'Docker',
-    //     level: 'beginner',
-    //     defaultGroup: 'devops',
-    //     companies: {
-    //       aprSystem: ['devops'],
-    //       primaris: [],
-    //     },
-    //   },
-    // },
-    // integration: {
-    //   snaplogic: {
-    //     label: 'SnapLogic',
-    //     level: 'beginner',
-    //     defaultGroup: 'integration',
-    //     companies: {
-    //       aprSystem: [],
-    //       primaris: ['integration'],
-    //     },
-    //   },
-    // },
-    // versionControlSystem: {
-    //   git: {
-    //     label: 'Git',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: ['common'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   github: {
-    //     label: 'GitHub',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: [],
-    //       primaris: ['common'],
-    //     },
-    //   },
-    //   gitlab: {
-    //     label: 'GitLab',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: ['common'],
-    //       primaris: [],
-    //     },
-    //   },
-    // },
-    // packageManager: {
-    //   npm: {
-    //     label: 'npm',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: ['common'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   pnpm: {
-    //     label: 'pnpm',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: ['common'],
-    //       primaris: [],
-    //     },
-    //   },
-    // },
-    // codeQuality: {
-    //   eslint: {
-    //     label: 'Eslint',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: ['common'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   prettier: {
-    //     label: 'Prettier',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: ['common'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   stylelint: {
-    //     label: 'Stylelint',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: ['common'],
-    //       primaris: [],
-    //     },
-    //   },
-    // },
-    // developerTool: {
-    //   postman: {
-    //     label: 'Postman',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: ['common'],
-    //       primaris: ['common'],
-    //     },
-    //   },
-    //   nx: {
-    //     label: 'Nx',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: ['common'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   enterpriseArchitect: {
-    //     label: 'Enterprise Architect',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: ['common'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   webstorm: {
-    //     label: 'WebStorm',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: ['common'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   visualStudioCode: {
-    //     label: 'Visual Studio Code',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: ['common'],
-    //       primaris: [],
-    //     },
-    //   },
-    //   eclipse: {
-    //     label: 'Eclipse',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: [],
-    //       primaris: [],
-    //     },
-    //   },
-    //   jira: {
-    //     label: 'Jira',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       aprSystem: [],
-    //       primaris: ['common'],
-    //     },
-    //   },
-    // },
-    // operatingSystem: {
-    //   windows: {
-    //     label: 'Windows',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       primaris: ['common'],
-    //       aprSystem: ['common'],
-    //     },
-    //   },
-    //   linux: {
-    //     label: 'Linux',
-    //     level: 'beginner',
-    //     defaultGroup: 'common',
-    //     companies: {
-    //       primaris: [],
-    //       aprSystem: [],
-    //     },
-    //   },
-    // },
+    markupLanguage: {
+      html: {
+        name: 'HTML',
+        level: 'beginner',
+        defaultGroup: 'frontend',
+        companies: {
+          aprSystem: ['frontend'],
+          primaris: [],
+        },
+      },
+    },
+    stylesheetLanguage: {
+      css: {
+        name: 'CSS',
+        level: 'beginner',
+        defaultGroup: 'frontend',
+        companies: {
+          aprSystem: ['frontend'],
+          primaris: [],
+        },
+      },
+      scss: {
+        name: 'SCSS',
+        level: 'beginner',
+        defaultGroup: 'frontend',
+        companies: {
+          aprSystem: ['frontend'],
+          primaris: [],
+        },
+      },
+    },
+    programmingLanguage: {
+      javascript: {
+        name: 'JavaScript',
+        level: 'beginner',
+        defaultGroup: 'frontend',
+        companies: {
+          aprSystem: ['frontend', 'backend'],
+          primaris: ['integration'],
+        },
+      },
+      typescript: {
+        name: 'TypeScript',
+        level: 'beginner',
+        defaultGroup: 'frontend',
+        companies: {
+          aprSystem: ['frontend', 'backend'],
+          primaris: [],
+        },
+      },
+      java: {
+        name: 'Java',
+        level: 'beginner',
+        defaultGroup: 'backend',
+        companies: {
+          aprSystem: [],
+          primaris: [],
+        },
+      },
+      python: {
+        name: 'Python',
+        level: 'beginner',
+        defaultGroup: 'ai',
+        companies: {
+          aprSystem: [],
+          primaris: [],
+        },
+      },
+    },
+    framework: {
+      angular: {
+        name: 'Angular',
+        level: 'beginner',
+        defaultGroup: 'frontend',
+        companies: {
+          aprSystem: ['frontend'],
+          primaris: [],
+        },
+      },
+    },
+    library: {
+      rxjs: {
+        name: 'RxJS',
+        level: 'beginner',
+        defaultGroup: 'frontend',
+        companies: {
+          aprSystem: ['frontend'],
+          primaris: [],
+        },
+      },
+      ngxs: {
+        name: 'NGXS',
+        level: 'beginner',
+        defaultGroup: 'frontend',
+        companies: {
+          aprSystem: ['frontend'],
+          primaris: [],
+        },
+      },
+      storybook: {
+        name: 'Storybook',
+        level: 'beginner',
+        defaultGroup: 'frontend',
+        companies: {
+          aprSystem: ['frontend'],
+          primaris: [],
+        },
+      },
+      primeng: {
+        name: 'PrimeNG',
+        level: 'beginner',
+        defaultGroup: 'frontend',
+        companies: {
+          aprSystem: ['frontend'],
+          primaris: [],
+        },
+      },
+    },
+    test: {
+      jest: {
+        name: 'Jest',
+        level: 'beginner',
+        defaultGroup: 'frontend',
+        companies: {
+          aprSystem: ['frontend'],
+          primaris: [],
+        },
+      },
+    },
+    database: {
+      postgresql: {
+        name: 'PostgreSQL',
+        level: 'beginner',
+        defaultGroup: 'backend',
+        companies: {
+          aprSystem: ['backend'],
+          primaris: [],
+        },
+      },
+      microsoftSqlServer: {
+        name: 'Microsoft SQL Server',
+        level: 'beginner',
+        defaultGroup: 'integration',
+        companies: {
+          aprSystem: [],
+          primaris: ['integration'],
+        },
+      },
+    },
+    server: {
+      nginx: {
+        name: 'NGINX',
+        level: 'beginner',
+        defaultGroup: 'devops',
+        companies: {
+          aprSystem: [],
+          primaris: [],
+        },
+      },
+    },
+    runtime: {
+      nodejs: {
+        name: 'Node.js',
+        level: 'beginner',
+        defaultGroup: 'backend',
+        companies: {
+          aprSystem: ['backend'],
+          primaris: [],
+        },
+      },
+    },
+    containerization: {
+      docker: {
+        name: 'Docker',
+        level: 'beginner',
+        defaultGroup: 'devops',
+        companies: {
+          aprSystem: ['devops'],
+          primaris: [],
+        },
+      },
+    },
+    integration: {
+      snaplogic: {
+        name: 'SnapLogic',
+        level: 'beginner',
+        defaultGroup: 'integration',
+        companies: {
+          aprSystem: [],
+          primaris: ['integration'],
+        },
+      },
+    },
+    versionControlSystem: {
+      git: {
+        name: 'Git',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: ['common'],
+          primaris: [],
+        },
+      },
+      github: {
+        name: 'GitHub',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: [],
+          primaris: ['common'],
+        },
+      },
+      gitlab: {
+        name: 'GitLab',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: ['common'],
+          primaris: [],
+        },
+      },
+    },
+    packageManager: {
+      npm: {
+        name: 'npm',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: ['common'],
+          primaris: [],
+        },
+      },
+      pnpm: {
+        name: 'pnpm',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: ['common'],
+          primaris: [],
+        },
+      },
+    },
+    codeQuality: {
+      eslint: {
+        name: 'Eslint',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: ['common'],
+          primaris: [],
+        },
+      },
+      prettier: {
+        name: 'Prettier',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: ['common'],
+          primaris: [],
+        },
+      },
+      stylelint: {
+        name: 'Stylelint',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: ['common'],
+          primaris: [],
+        },
+      },
+    },
+    developerTool: {
+      postman: {
+        name: 'Postman',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: ['common'],
+          primaris: ['common'],
+        },
+      },
+      nx: {
+        name: 'Nx',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: ['common'],
+          primaris: [],
+        },
+      },
+      enterpriseArchitect: {
+        name: 'Enterprise Architect',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: ['common'],
+          primaris: [],
+        },
+      },
+      webstorm: {
+        name: 'WebStorm',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: ['common'],
+          primaris: [],
+        },
+      },
+      visualStudioCode: {
+        name: 'Visual Studio Code',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: ['common'],
+          primaris: [],
+        },
+      },
+      eclipse: {
+        name: 'Eclipse',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: [],
+          primaris: [],
+        },
+      },
+      jira: {
+        name: 'Jira',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          aprSystem: [],
+          primaris: ['common'],
+        },
+      },
+    },
+    operatingSystem: {
+      windows: {
+        name: 'Windows',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          primaris: ['common'],
+          aprSystem: ['common'],
+        },
+      },
+      linux: {
+        name: 'Linux',
+        level: 'beginner',
+        defaultGroup: 'common',
+        companies: {
+          primaris: [],
+          aprSystem: [],
+        },
+      },
+    },
   },
 })
 @Injectable()
