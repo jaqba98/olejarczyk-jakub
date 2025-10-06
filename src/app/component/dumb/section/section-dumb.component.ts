835573e2111074aa912625bfb6e829eb231726bd
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-06 12:21:29 +0200

    Add max width and margin auto to the section

--- src/app/component/dumb/section/section-dumb.component.ts
+++ src/app/component/dumb/section/section-dumb.component.ts
@@ -1,6 +1,9 @@
 import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ColorType } from '../../../type/color.type';
+import { BemUtil } from '../../../util/bem.util';
+
 @Component({
   selector: 'section-dumb',
   templateUrl: './section-dumb.component.html',
@@ -8,9 +11,24 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
 })
 export class SectionDumbComponent {
-  color = input<'dominant' | 'complementary' | 'accent'>('dominant');
+  color = input.required<ColorType>();
 
-  buildClassNames() {
-    return [`section__background--${this.color()}`];
+  maxWidth960px = input(false);
+
+  marginAuto = input(false);
+
+  constructor(private readonly bemUti: BemUtil) {}
+
+  buildMainClassNames() {
+    return {
+      [this.bemUti.build('section', 'background', this.color())]: true,
+    };
+  }
+
+  buildContentClassNames() {
+    return {
+      [this.bemUti.build('section', 'content', 'max-width-960px')]: this.maxWidth960px(),
+      [this.bemUti.build('section', 'content', 'margin-auto')]: this.marginAuto(),
+    };
   }
 }
---
+++ src/app/util/bem.util.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class BemUtil {
+  build(block: string, element: string, modifier: string) {
+    if (block === '') {
+      throw new Error('The block can not be empty!');
+    }
+    if (element !== '' && modifier !== '') {
+      return `${block}__${element}--${modifier}`;
+    }
+    if (element !== '' && modifier === '') {
+      return `${block}__${element}`;
+    }
+    if (element === '' && modifier !== '') {
+      return `${block}--${modifier}`;
+    }
+    return block;
+  }
+}
