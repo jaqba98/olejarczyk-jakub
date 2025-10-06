523aeb17fe59664c1ea2fe92d43ab00c9f7ef233
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-06 15:29:47 +0200

    Create content component

---
+++ src/app/component/dumb/content/content-dumb.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'content-dumb',
+  templateUrl: './content-dumb.component.html',
+  styleUrl: './content-dumb.component.scss',
+})
+export class ContentDumbComponent {}
--- src/app/component/view/experience/experience-view.component.ts
+++ src/app/component/view/experience/experience-view.component.ts
@@ -5,12 +5,19 @@ import { ExperienceBuilder } from '../../../builder/experience.builder';
 import { ParagraphDumbComponent } from '../../dumb/paragraph/paragraph-dumb.component';
 import { SmallDumbComponent } from '../../dumb/small/small-dumb.component';
 import { SectionDumbComponent } from '../../dumb/section/section-dumb.component';
+import { ContentDumbComponent } from '../../dumb/content/content-dumb.component';
 
 @Component({
   selector: 'experience-view',
   templateUrl: './experience-view.component.html',
   styleUrl: './experience-view.component.scss',
-  imports: [CommonModule, ParagraphDumbComponent, SmallDumbComponent, SectionDumbComponent],
+  imports: [
+    CommonModule,
+    ParagraphDumbComponent,
+    SmallDumbComponent,
+    SectionDumbComponent,
+    ContentDumbComponent,
+  ],
   providers: [ExperienceBuilder],
 })
 export class ExperienceViewComponent {
