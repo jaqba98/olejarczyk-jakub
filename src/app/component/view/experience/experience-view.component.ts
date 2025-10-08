faa8a79478442818b47174266b07709718dfef78
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-08 15:20:36 +0200

    Create flex item dumb

--- src/app/component/view/experience/experience-view.component.ts
+++ src/app/component/view/experience/experience-view.component.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { ExperienceBuilder } from '../../../builder/experience.builder';
 import { SectionDumbComponent } from '../../dumb/section/section-dumb.component';
+import { FlexDumbComponent } from '../../dumb/flex/flex-dumb.component';
+import { FlexItemDumbComponent } from '../../dumb/flex-item/flex-item-dumb.component';
 
 @Component({
   selector: 'experience-view',
   templateUrl: './experience-view.component.html',
   styleUrl: './experience-view.component.scss',
-  imports: [CommonModule, SectionDumbComponent],
+  imports: [CommonModule, SectionDumbComponent, FlexDumbComponent, FlexItemDumbComponent],
   providers: [ExperienceBuilder],
 })
 export class ExperienceViewComponent {
--- src/app/component/view/technology/technology-view.component.ts
+++ src/app/component/view/technology/technology-view.component.ts
@@ -3,12 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { TechnologyBuilder } from '../../../builder/technology.builder';
 import { SectionDumbComponent } from '../../dumb/section/section-dumb.component';
+import { FlexDumbComponent } from '../../dumb/flex/flex-dumb.component';
+import { FlexItemDumbComponent } from '../../dumb/flex-item/flex-item-dumb.component';
+import { HeaderDumbComponent } from '../../dumb/header/header-dumb.component';
 
 @Component({
   selector: 'technology-view',
   templateUrl: './technology-view.component.html',
   styleUrl: './technology-view.component.scss',
-  imports: [CommonModule, SectionDumbComponent],
+  imports: [
+    CommonModule,
+    SectionDumbComponent,
+    FlexDumbComponent,
+    FlexItemDumbComponent,
+    HeaderDumbComponent,
+  ],
   providers: [TechnologyBuilder],
 })
 export class TechnologyViewComponent {
