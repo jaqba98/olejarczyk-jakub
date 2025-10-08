83954ced2efab471023e5db4140f3d2a1d8c2d03
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-08 11:15:41 +0200

    Create basic section structure

--- src/app/component/dumb/section/section-dumb.component.ts
+++ src/app/component/dumb/section/section-dumb.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'section-dumb',
   templateUrl: './section-dumb.component.html',
   styleUrl: './section-dumb.component.scss',
 })
-export class SectionDumbComponent {}
+export class SectionDumbComponent {
+  id = input.required<string>();
+
+  title = input.required<string>();
+}
