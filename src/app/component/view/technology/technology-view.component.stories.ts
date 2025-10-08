834eea4f11001ffcf5a3c8017d43730b41c25eff
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-08 09:21:12 +0200

    Create an empty technology view component

---
+++ src/app/component/view/technology/technology-view.component.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { TechnologyViewComponent } from './technology-view.component';
+
+const meta: Meta<TechnologyViewComponent> = {
+  title: 'Component/View/Technology',
+  component: TechnologyViewComponent,
+};
+
+export default meta;
+type Story = StoryObj<TechnologyViewComponent>;
+
+export const Primary: Story = {};
---
+++ src/app/component/view/technology/technology-view.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'technology-view',
+  templateUrl: './technology-view.component.html',
+  styleUrl: './technology-view.component.scss',
+})
+export class TechnologyViewComponent {}
