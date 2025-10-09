e13258fdaa97ad74f704bccb7e8b0fa81cd9c0b7
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-09 11:57:41 +0200

    Display the content of technology view

--- src/app/component/view/technology/technology-view.component.ts
+++ src/app/component/view/technology/technology-view.component.ts
@@ -3,16 +3,28 @@ import { Component } from '@angular/core';
 
 import { TechnologyBuilder } from '../../../builder/technology.builder';
 import { SectionBlockComponent } from '../../block/section/section-block.component';
+import { HeadingTextComponent } from '../../text/heading/heading-text.component';
+import { ColorType } from '../../../type/color.type';
+import { FlexItemUtilComponent } from '../../util/flex-item/flex-item-util.component';
+import { FlexUtilComponent } from '../../util/flex/flex-util.component';
 
 @Component({
   selector: 'technology-view',
   templateUrl: './technology-view.component.html',
   styleUrl: './technology-view.component.scss',
-  imports: [CommonModule, SectionBlockComponent],
+  imports: [
+    CommonModule,
+    SectionBlockComponent,
+    HeadingTextComponent,
+    FlexItemUtilComponent,
+    FlexUtilComponent,
+  ],
   providers: [TechnologyBuilder],
 })
 export class TechnologyViewComponent {
   technology$;
+
+  rootColor: ColorType = 'accent';
 
   constructor(private readonly builder: TechnologyBuilder) {
     this.technology$ = this.builder.build();
