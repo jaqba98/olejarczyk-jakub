2a22ddf50f8558d10ec58f3d9bb944b4aa47e572
jakub <jakubolejarczyk98@gmail.com>
2025-10-13 15:41:45 +0200

    Add headers

--- src/app/component/view/technology/technology-view.component.ts
+++ src/app/component/view/technology/technology-view.component.ts
@@ -5,11 +5,18 @@ import { TechnologyBuilder } from '../../../builder/technology.builder';
 import { SectionBlockComponent } from '../../block/section/section-block.component';
 import { FlexUtilComponent } from '../../util/flex/flex-util.component';
 import { FlexItemUtilComponent } from '../../util/flex-item/flex-item-util.component';
+import { HeadingTextComponent } from '../../text/heading/heading-text.component';
 
 @Component({
   selector: 'technology-view',
   templateUrl: './technology-view.component.html',
-  imports: [CommonModule, SectionBlockComponent, FlexUtilComponent, FlexItemUtilComponent],
+  imports: [
+    CommonModule,
+    SectionBlockComponent,
+    FlexUtilComponent,
+    FlexItemUtilComponent,
+    HeadingTextComponent,
+  ],
   providers: [TechnologyBuilder],
 })
 export class TechnologyViewComponent {
