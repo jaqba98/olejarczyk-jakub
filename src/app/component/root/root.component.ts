d26d4433b3a7e0000084262e642810c1dbd11ce7
jakub <jakubolejarczyk98@gmail.com>
2025-10-16 15:25:03 +0200

    Create basic nav component

--- src/app/component/root/root.component.ts
+++ src/app/component/root/root.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 import { TechnologyViewComponent } from '../view/technology/technology-view.component';
 import { ExperienceViewComponent } from '../view/experience/experience-view.component';
+import { NavViewComponent } from '../view/nav/nav-view.component';
 
 @Component({
   selector: 'root',
   templateUrl: './root.component.html',
-  imports: [TechnologyViewComponent, ExperienceViewComponent],
+  imports: [TechnologyViewComponent, ExperienceViewComponent, NavViewComponent],
 })
 export class RootComponent {}
---
+++ src/app/component/view/nav/nav-view.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nav-view',
+  templateUrl: './nav-view.component.html',
+})
+export class NavViewComponent {}
