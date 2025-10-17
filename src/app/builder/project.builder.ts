061219695a52f5036adb47dfa8b0e5a43b39303a
jakub <jakubolejarczyk98@gmail.com>
2025-10-17 15:53:09 +0200

    Create basic project builder

---
+++ src/app/builder/project.builder.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { map } from 'rxjs/operators';
+
+import { ProjectState } from '../state/project/project.state';
+import { ProjectStateModel } from '../state/project/project-state.model';
+import { ProjectDomainType } from '../domain/type/project-domain.type';
+
+@Injectable()
+export class ProjectBuilder {
+  constructor(private readonly store: Store) {}
+
+  build() {
+    return this.store
+      .selectOnce(ProjectState.getState)
+      .pipe(map((state) => this.buildProject(state)));
+  }
+
+  private buildProject(state: ProjectStateModel) {
+    return Object.entries(state).map((array) => ({
+      projectType: array[0] as ProjectDomainType,
+      projectModel: array[1],
+    }));
+  }
+}
---
+++ src/app/component/view/project/project-view.component.stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { ProjectViewComponent } from './project-view.component';
+import { StoreUtil } from '../../../util/store.util';
+
+const meta: Meta<ProjectViewComponent> = {
+  title: 'component/view/project',
+  component: ProjectViewComponent,
+  decorators: [...StoreUtil.getStorybookProvide()],
+};
+
+export default meta;
+type Story = StoryObj<ProjectViewComponent>;
+
+export const Primary: Story = {};
