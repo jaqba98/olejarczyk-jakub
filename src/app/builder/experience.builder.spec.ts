198558e1ce153be632ae6530d8f218f290c494e1
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-20 14:46:13 +0200

    Create basic experience test

--- src/app/builder/experience.builder.spec.ts
+++ src/app/builder/experience.builder.spec.ts
@@ -1,5 +1,27 @@
+import { Store } from '@ngxs/store';
+import { TestBed } from '@angular/core/testing';
+import { firstValueFrom } from 'rxjs';
+
+import { StoreUtil } from '../util/store.util';
+import { ExperienceBuilder } from './experience.builder';
+
 describe('Experience builder', () => {
-  it('true === true', () => {
-    expect(true).toBeTruthy();
+  let store: Store;
+
+  beforeAll(() => {
+    TestBed.configureTestingModule({
+      providers: [StoreUtil.getProvide(), ExperienceBuilder],
+    });
+    store = TestBed.inject(Store);
+  });
+
+  afterAll(() => {
+    store.reset(null);
+  });
+
+  it('Should return correct experience data', async () => {
+    const service = TestBed.inject(ExperienceBuilder);
+    const result = await firstValueFrom(service.build());
+    expect(result).toBe([]);
   });
 });
