578f37ab03c4749d003e262a934d6785a6915eda
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-21 10:40:28 +0200

    Create project data and tests

---
+++ src/app/domain/const/data/project-data-domain.const.ts
@@ -0,0 +1,58 @@
+import { ProjectStateDomainModel } from '../../model/state/project-state-domain.model';
+
+export const projectDataDomainConst: ProjectStateDomainModel = {
+  project1: {
+    id: 'project1',
+    order: 0,
+    banner: 'assetImage1',
+    name: 'Project 1',
+    creationDate: new Date(2025, 1, 1),
+    status: 'inProgress',
+    description: 'Project 1 lorem ipsum',
+    technologyIds: ['html', 'css', 'javascript'],
+    repository: {
+      type: 'github',
+      url: 'https://github.com/username/project1',
+    },
+    application: {
+      type: 'githubPage',
+      url: 'https://username.github.io/project1',
+    },
+    gallery: ['assetImage1', 'assetImage2', 'assetImage3'],
+  },
+  project2: {
+    id: 'project2',
+    order: 1,
+    banner: 'assetImage2',
+    name: 'Project 2',
+    creationDate: new Date(2024, 1, 1),
+    status: 'completed',
+    description: 'Project 2 lorem ipsum',
+    technologyIds: ['git', 'github', 'gitlab'],
+    repository: {
+      type: 'github',
+      url: 'https://github.com/username/project2',
+    },
+    application: {
+      type: 'none',
+    },
+    gallery: ['assetImage1', 'assetImage2', 'assetImage3'],
+  },
+  project3: {
+    id: 'project3',
+    order: 2,
+    banner: 'assetImage3',
+    name: 'Project 3',
+    creationDate: new Date(2023, 1, 1),
+    status: 'completed',
+    description: 'Project 3 lorem ipsum',
+    technologyIds: ['snaplogic', 'javascript'],
+    repository: {
+      type: 'none',
+    },
+    application: {
+      type: 'none',
+    },
+    gallery: ['assetImage1', 'assetImage2', 'assetImage3'],
+  },
+};
--- src/app/domain/model/state/project-state-domain.model.ts
+++ src/app/domain/model/state/project-state-domain.model.ts
@@ -5,14 +5,40 @@ import { TechnologyIdDomainType } from '../../type/id/technology-id-domain.type'
 import { ProjectNameDomainType } from '../../type/name/project-name-domain.type';
 import { BaseStateDomainModel } from '../base/base-state-domain.model';
 
-export interface ProjectStateDomainModel extends BaseStateDomainModel<ProjectIdDomainType> {
+interface ProjectRepositoryNoneDomainModel {
+  type: 'none';
+}
+
+interface ProjectRepositoryGitHubDomainModel {
+  type: 'github';
+  url: string;
+}
+
+type ProjectRepositoryModel = ProjectRepositoryNoneDomainModel | ProjectRepositoryGitHubDomainModel;
+
+interface ProjectApplicationNoneDomainModel {
+  type: 'none';
+}
+
+interface ProjectApplicationGitHubPageDomainModel {
+  type: 'githubPage';
+  url: string;
+}
+
+type ProjectApplicationModel =
+  | ProjectApplicationNoneDomainModel
+  | ProjectApplicationGitHubPageDomainModel;
+
+export interface ProjectDomainModel extends BaseStateDomainModel<ProjectIdDomainType> {
   banner: ImageAssetDomainType;
   name: ProjectNameDomainType;
   creationDate: Date;
   status: StatusEntityDomainType;
   description: string;
   technologyIds: TechnologyIdDomainType[];
-  repositoryUrl: string;
-  applicationUrl: string;
+  repository: ProjectRepositoryModel;
+  application: ProjectApplicationModel;
   gallery: ImageAssetDomainType[];
 }
+
+export type ProjectStateDomainModel = Record<ProjectIdDomainType, ProjectDomainModel>;
