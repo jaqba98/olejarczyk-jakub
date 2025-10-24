30a7729797107a94fdfdaabdd92568705bac76b5
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-24 20:11:49 +0200

    Remove the technology group

--- src/app/data/model/technology-group-data.model.ts
+++
@@ -1,6 +0,0 @@
-import { TechnologyGroupIdDataType } from '../type/id/technology-group-id-data.type';
-import { BaseDataModel } from './base-data.model';
-
-export interface TechnologyGroupModel extends BaseDataModel<TechnologyGroupIdDataType> {}
-
-export type TechnologyGroupDataModel = Record<TechnologyGroupIdDataType, TechnologyGroupModel>;
--- src/app/data/type/id/technology-group-id-data.type.ts
+++
@@ -1,7 +0,0 @@
-export type TechnologyGroupIdDataType =
-  | 'frontend'
-  | 'backend'
-  | 'devops'
-  | 'integration'
-  | 'ai'
-  | 'common';
