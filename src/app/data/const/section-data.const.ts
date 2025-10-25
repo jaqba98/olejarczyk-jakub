7fd1d78513b31cb7b165d62bb1bb4c310d2ef644
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-10-25 08:32:47 +0200

    Refactor the section data

--- src/app/data/const/section-data.const.ts
+++ src/app/data/const/section-data.const.ts
@@ -3,74 +3,50 @@ import { SectionDataModel } from '../model/section-data.model';
 export const sectionDataConst: SectionDataModel = {
   nav: {
     id: 'nav',
-    order: 0,
     label: 'Nav',
-    href: '#nav',
   },
   home: {
     id: 'home',
-    order: 1,
     label: 'Home',
-    href: '#home',
   },
   aboutMe: {
-    id: 'aboutMe',
-    order: 2,
+    id: 'about-me',
     label: 'About me',
-    href: '#about-me',
   },
   technology: {
     id: 'technology',
-    order: 3,
     label: 'Technology',
-    href: '#technology',
   },
   skill: {
     id: 'skill',
-    order: 4,
     label: 'Skill',
-    href: '#skill',
   },
   experience: {
     id: 'experience',
-    order: 5,
     label: 'Experience',
-    href: '#experience',
   },
   resume: {
     id: 'resume',
-    order: 6,
     label: 'Resume',
-    href: '#resume',
   },
   education: {
     id: 'education',
-    order: 7,
     label: 'Education',
-    href: '#education',
   },
   project: {
     id: 'project',
-    order: 8,
     label: 'Project',
-    href: '#project',
   },
   blog: {
     id: 'blog',
-    order: 9,
     label: 'Blog',
-    href: '#blog',
   },
   contact: {
     id: 'contact',
-    order: 10,
     label: 'Contact',
-    href: '#contact',
   },
   footer: {
     id: 'footer',
-    order: 11,
     label: 'Footer',
-    href: '#footer',
   },
 };
