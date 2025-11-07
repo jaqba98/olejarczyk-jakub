16f9115815a90f023809f72a2053f292cf3c37ca
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-11-07 21:51:22 +0100

    Add storybook provider to the sottrybook

--- src/app/component/root/root.component.stories.ts
+++ src/app/component/root/root.component.stories.ts
@@ -1,10 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 
 import { RootComponent } from './root.component';
+import { StoreProvider } from '../../store/provider/store.provider';
 
 const meta: Meta<RootComponent> = {
   title: 'component/root',
   component: RootComponent,
+  decorators: StoreProvider.getStorybookProvider(),
 };
 
 export default meta;
