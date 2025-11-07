653ec3006e2d946f1dfe5c32157558ea56b0ac0d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-11-07 21:58:51 +0100

    Render all section ids

--- src/app/component/root/root.component.ts
+++ src/app/component/root/root.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { CommonModule } from '@angular/common';
+import { map } from 'rxjs/operators';
+
+import { SectionStoreState } from '../../store/state/section-store.state';
 
 @Component({
   selector: 'root-component',
   templateUrl: './root.component.html',
+  imports: [CommonModule],
 })
-export class RootComponent {}
+export class RootComponent {
+  sections$;
+
+  constructor(private readonly store: Store) {
+    this.sections$ = this.store
+      .select(SectionStoreState.getState)
+      .pipe(map((state) => state.layout.sections));
+  }
+}
