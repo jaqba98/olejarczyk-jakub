bc048c0c7378cf5441afb73644ff704c4816c17d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-11-07 21:45:42 +0100

    Clean up the root component

--- src/app/component/root/root.component.ts
+++ src/app/component/root/root.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { SectionComponent } from '../section/section.component';
 
 @Component({
   selector: 'root-component',
   templateUrl: './root.component.html',
-  imports: [SectionComponent],
 })
 export class RootComponent {}
