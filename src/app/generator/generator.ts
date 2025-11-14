9f90b15146faf322eac4f735f5cb70149f0a294a
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-11-14 17:09:42 +0100

    Fixes generator

--- src/app/generator/generator.ts
+++ src/app/generator/generator.ts
@@ -1,25 +1,70 @@
-import { Component, input } from '@angular/core';
-import { GroupLayoutModel, LayoutModel, LeafLayoutModel } from '../layout/model/layout.model';
+import { Component, input, ViewChild, ViewContainerRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-// import { GroupLayoutModel, LayoutModel, LeafLayoutModel } from '../../layout/model/layout.model';
-// import { LeafComponent } from '../layout/leaf/leaf.component';
-// import { GroupComponent } from '../layout/group/group.component';
+import { LayoutModel } from '../layout/model/layout.model';
 
 @Component({
   selector: 'generator',
   templateUrl: './generator.html',
-  // imports: [CommonModule, LeafComponent, GroupComponent],
+  imports: [CommonModule],
 })
 export class Generator {
+  @ViewChild('generator', { read: ViewContainerRef })
+  generator!: ViewContainerRef;
+
   model = input.required<LayoutModel>();
 
-  get leaf(): LeafLayoutModel | null {
-    const model = this.model();
-    return model.kind === 'leaf' ? model : null;
-  }
+  // ngOnInit() {
+  //   this.generator.clear();
+  //   const model = this.model();
+  //   switch (model.kind) {
+  //     case 'leaf':
+  //       const component = getDomainComponent(model.domain.kind);
+  //       const ref: any = this.generator.createComponent(component);
+  //       ref.instance.data = model.domain.data;
+  //       ref.instance.metadata = model.domain.metadata;
+  //       ref.instance.appearance = model.domain.appearance;
+  //       ref.instance.children = [];
+  //       ref.changeDetectorRef.detectChanges();
+  //       break;
+  //     case 'group':
+  //       const { domain, children } = model;
+  //       if (domain) {
+  //         const component = getDomainComponent(domain.kind);
+  //         const ref: any = this.generator.createComponent(component);
+  //         ref.instance.data = domain.data;
+  //         ref.instance.metadata = domain.metadata;
+  //         ref.instance.appearance = domain.appearance;
+  //         ref.instance.children = children;
+  //         ref.changeDetectorRef.detectChanges();
+  //       } else {
+  //         children.forEach((child) => {
+  //           const ref = this.generator.createComponent(Generator);
+  //           ref.instance.model = child;
+  //           ref.changeDetectorRef.detectChanges();
+  //         });
+  //       }
+  //       break;
+  //     default:
+  //       throw new Error('Not supported kind!');
+  //   }
+  // }
 
-  get group(): GroupLayoutModel | null {
-    const model = this.model();
-    return model.kind === 'group' ? model : null;
-  }
+  // getComponent() {
+  //   const { domain } = this.layout();
+  //   if (domain) {
+  //     return getDomainComponent(domain.kind);
+  //   }
+  //   throw new Error('The domain is not defined!');
+  // }
+
+  // getInputs() {
+  //   const selfLayoput = this.layout();
+  //   if (selfLayoput.domain) {
+  //     const { data, metadata, appearance } = selfLayoput.domain;
+  //     const children = selfLayoput.kind === 'group' ? selfLayoput.children : [];
+  //     return { data, metadata, appearance, children };
+  //   }
+  //   throw new Error('The domain is not defined!');
+  // }
 }
