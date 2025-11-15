317868ae645861ea99274641b1ddde0860bfacd0
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-11-15 20:28:04 +0100

    Remove the unnecesary property from the paragraph metadata

--- src/app/metadata/builder/paragraph-metadata.builder.ts
+++ src/app/metadata/builder/paragraph-metadata.builder.ts
@@ -1,69 +1,7 @@
 import { ParagraphMetadataModel } from '../model/paragraph-metadata.model';
 
 export class ParagraphMetadataBuilder {
-  static buildNav(): ParagraphMetadataModel {
-    return {
-      test: true,
-    };
-  }
-
-  static buildHome(): ParagraphMetadataModel {
-    return {
-      test: true,
-    };
-  }
-
-  static buildAboutMe(): ParagraphMetadataModel {
-    return {
-      test: true,
-    };
-  }
-
-  static buildTechnology(): ParagraphMetadataModel {
-    return {
-      test: true,
-    };
-  }
-
-  static buildSkill(): ParagraphMetadataModel {
-    return {
-      test: true,
-    };
-  }
-
-  static buildExperience(): ParagraphMetadataModel {
-    return {
-      test: true,
-    };
-  }
-
-  static buildResume(): ParagraphMetadataModel {
-    return {
-      test: true,
-    };
-  }
-
-  static buildEducation(): ParagraphMetadataModel {
-    return {
-      test: true,
-    };
-  }
-
-  static buildProject(): ParagraphMetadataModel {
-    return {
-      test: true,
-    };
-  }
-
-  static buildContact(): ParagraphMetadataModel {
-    return {
-      test: true,
-    };
-  }
-
-  static buildFooter(): ParagraphMetadataModel {
-    return {
-      test: true,
-    };
+  static buildFooterCopyright(): ParagraphMetadataModel {
+    return {};
   }
 }
--- src/app/metadata/model/paragraph-metadata.model.ts
+++ src/app/metadata/model/paragraph-metadata.model.ts
@@ -1,3 +1,1 @@
-export interface ParagraphMetadataModel {
-  test: boolean;
-}
+export interface ParagraphMetadataModel {}
