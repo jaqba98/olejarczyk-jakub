747862ce8652f2ea234a4f153c6c34cfed42c4d1
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-11-22 20:00:09 +0100

    Add shape styles

--- src/app/data/appearance/builder/section-appearance.builder.ts
+++ src/app/data/appearance/builder/section-appearance.builder.ts
@@ -7,47 +7,47 @@ export class SectionAppearanceBuilder {
   build(): SectionAppearanceModel {
     return {
       nav: {
-        main: ['color__brand'],
+        main: ['color__brand', 'shape__block'],
         container: [],
       },
       home: {
-        main: ['color__brand'],
+        main: ['color__brand', 'shape__content'],
         container: [],
       },
       aboutMe: {
-        main: ['color__content--light'],
+        main: ['color__content--light', 'shape__content'],
         container: [],
       },
       technology: {
-        main: ['color__content--soft'],
+        main: ['color__content--soft', 'shape__content'],
         container: [],
       },
       skill: {
-        main: ['color__content--light'],
+        main: ['color__content--light', 'shape__content'],
         container: [],
       },
       experience: {
-        main: ['color__content--soft'],
+        main: ['color__content--soft', 'shape__content'],
         container: [],
       },
       resume: {
-        main: ['color__content--light'],
+        main: ['color__content--light', 'shape__content'],
         container: [],
       },
       education: {
-        main: ['color__content--soft'],
+        main: ['color__content--soft', 'shape__content'],
         container: [],
       },
       project: {
-        main: ['color__content--light'],
+        main: ['color__content--light', 'shape__content'],
         container: [],
       },
       contact: {
-        main: ['color__content--soft'],
+        main: ['color__content--soft', 'shape__content'],
         container: [],
       },
       footer: {
-        main: ['color__content--dark'],
+        main: ['color__content--dark', 'shape__block'],
         container: [],
       },
     };
--- src/app/type/css/class-css.type.ts
+++ src/app/type/css/class-css.type.ts
@@ -2,4 +2,6 @@ export type ClassCssType =
   | 'color__brand'
   | 'color__content--light'
   | 'color__content--soft'
-  | 'color__content--dark';
+  | 'color__content--dark'
+  | 'shape__block'
+  | 'shape__content';
