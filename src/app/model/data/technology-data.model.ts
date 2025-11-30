fff965e5e8e5e257d37f114e908f3f15da0ed680
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-11-30 22:54:42 +0100

    Refactor the technology data model

--- src/app/model/data/technology-data.model.ts
+++ src/app/model/data/technology-data.model.ts
@@ -1,1 +1,9 @@
-export interface TechnologyDataModel {}
+import { TechnologyCategoryRawItemModel } from '../raw/technology-category-raw.model';
+import { TechnologyRawItemModel } from '../raw/technology-raw.model';
+
+export interface TechnologyDataItemModel {
+  category: TechnologyCategoryRawItemModel;
+  technologies: TechnologyRawItemModel[];
+}
+
+export type TechnologyDataModel = TechnologyDataItemModel[];
--- src/app/model/raw/technology-raw.model.ts
+++ src/app/model/raw/technology-raw.model.ts
@@ -1,3 +1,4 @@
+import { TechnologyCategoryKindType } from '../../type/kind/technology-category-kind.type';
 import { TechnologyKindType } from '../../type/kind/technology-kind.type';
 import { TechnologyLevelType } from '../../type/level/technology-level.type';
 import { SvgModel } from '../svg/svg.model';
@@ -6,6 +7,7 @@ export interface TechnologyRawItemModel {
   label: string;
   level: TechnologyLevelType;
   icon: SvgModel;
+  categoryKind: TechnologyCategoryKindType;
 }
 
 export type TechnologyRawModel = Record<TechnologyKindType, TechnologyRawItemModel>;
